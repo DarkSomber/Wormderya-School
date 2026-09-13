8bf38dc5a61ea5d636c6f556b0f7ffd3ee73dcc0
Ysabella Rota <rotaysabelladylan@gmail.com>
2026-09-13 21:19:50 +0800

    Backstory screens

--- screens/StoryBackstoryScreen.js
+++ screens/StoryBackstoryScreen.js
@@ -17,11 +17,26 @@ import SlideshowSequence from "../components/SlideshowSequence";
 // One entry per slide. Add the real art later like:
 //   { id: '1', image: require('../../assets/story/backstory-1.png') }
 const BACKSTORY_SLIDES = [
-  { id: "1" },
-  { id: "2" },
-  { id: "3" },
-  { id: "4" },
-  { id: "5" },
+  {
+    id: "1",
+    image: require("../assets/Final/BackstoryScreens/Backstory-Panel-1.1.png"),
+  },
+  {
+    id: "2",
+    image: require("../assets/Final/BackstoryScreens/Backstory-Panel-1.2.png"),
+  },
+  {
+    id: "3",
+    image: require("../assets/Final/BackstoryScreens/Backstory-Panel-2.png"),
+  },
+  {
+    id: "4",
+    image: require("../assets/Final/BackstoryScreens/Backstory-Panel-3.png"),
+  },
+  {
+    id: "5",
+    image: require("../assets/Final/BackstoryScreens/Backstory-Panel-4.png"),
+  },
 ];
 
 export default function StoryBackstoryScreen({ onFinish }) {
